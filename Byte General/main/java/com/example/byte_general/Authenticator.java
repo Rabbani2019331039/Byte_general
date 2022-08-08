29332a4e837aa2756a7816d7be6a41b9aff7c3d8
Fathoor Rabbani <69500839+Rabbani2019331039@users.noreply.github.com>
2022-08-09 00:15:00 +0600

    Delete Byte General/main directory

--- Byte General/main/java/com/example/byte_general/Authenticator.java
+++
@@ -1,7 +0,0 @@
-package com.example.byte_general;
-
-public class Authenticator {
-    public static boolean authenticate(String username, String password) {
-        return username.equals("admin") && password.equals("admin");
-    }
-}
